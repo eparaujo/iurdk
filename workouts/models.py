1c9935239c5a1a96b96011832fcf65e9874fe1c2
Eduardo <epa.edu@gmail.com>
2024-10-01 12:05:43 -0300

    atualização de retorno

--- workouts/models.py
+++ workouts/models.py
@@ -7,3 +7,6 @@ class Workout(models.Model):
     style =  models.ForeignKey(KarateStyle, on_delete=models.DO_NOTHING, related_name='workout_style')
     description = models.TextField(blank=True, null=True)
     movie_workout = models.FileField(upload_to='workout/', blank=True, null=True)
+
+    def __str__(self):
+        return self.description
